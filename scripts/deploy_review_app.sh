e300d26c1f728553f115355df8cdd50541a65575
Jess <jesszeroual@gmail.com>
2019-12-09 23:38:48 +0100

    replace app-name in scripts

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"app-name-$TRAVIS_BRANCH\" }" > now-review-app.json
+echo "{ \"alias\": \"jesszeroual-$TRAVIS_BRANCH\" }" > now-review-app.json
 npx now --public --token $NOW_TOKEN
 npx now alias --token $NOW_TOKEN --local-config now-review-app.json
